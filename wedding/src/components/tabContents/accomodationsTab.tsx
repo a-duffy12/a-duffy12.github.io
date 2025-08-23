47563226b6121050e51420b38228b1e989c00e8e
Aiden <12aduffy@gmail.com>
2025-08-23 14:47:43 -0400

    map iframe logic and styles

--- wedding/src/components/tabContents/accomodationsTab.tsx
+++ wedding/src/components/tabContents/accomodationsTab.tsx
@@ -1,5 +1,22 @@
+import { Content } from '../content';
+import styles from '../../wedding.module.css';
+import { useMobile } from '../../hooks/useMobile';
+
 export const AccomodationsTab = () => {
+    const visitListowelLink = 'https://listowelcountryinn.com/visiting-listowel-country-inn/';
+
     return (
-        <div>Accomodations</div>
+        <Content>
+            <div className={styles.contentTwoColumn}>
+                <div className={styles.contentColumn}>
+                    <a href={visitListowelLink}>Listowel Tourism</a>
+                </div>
+                <div className={styles.contentColumn}>
+                    <div className={styles.mapContainer}>
+                        <iframe className={styles.mapFrame} src='https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=+(Listowel%20Country%20Inn)&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=B&amp;output=embed'/>
+                    </div>
+                </div>
+            </div>
+        </Content>
     )
 }

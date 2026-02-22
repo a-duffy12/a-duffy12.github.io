a412dc40ad785e97896e2d42a8aed11b2e5f206b
Aiden <12aduffy@gmail.com>
2026-02-22 17:26:32 -0500

    update FAQ with arrival times, make desktop view use columns

--- src/components/tabContents/faqTab.tsx
+++ src/components/tabContents/faqTab.tsx
@@ -2,10 +2,15 @@ import { Bar } from '../bar';
 import { Content } from '../content';
 import styles from '../../wedding.module.css';
 import { QnA } from '../qna';
+import { useMobile } from '../../hooks/useMobile';
 
 export const FaqTab = () => {
+    const {isMobile} = useMobile();
+
     return (
-        <Content>
+        <>
+        {isMobile ?
+            <Content>
             <h3 className={styles.contentText}>
                 {'Frequently Asked Questions'}
             </h3>
@@ -20,6 +25,14 @@ export const FaqTab = () => {
                     'The wedding colours are juniper and periwinkle.',
                     'The bridesmaids will be wearing juniper (green), and the groomsmen will be wearing gray.'
                 ]} 
+            />
+            <QnA
+                question={'When should I arrive?'}
+                answers={[
+                    'Ceremony - 4pm start - Arrive 15 to 30 minutes prior.',
+                    'Dinner - 6pm start - Arrive 10 to 15 minutes prior.',
+                    'Reception - 8pm start'
+                ]}
             />
             <QnA 
                 question={'Is everything at the same location?'} 
@@ -40,7 +53,6 @@ export const FaqTab = () => {
                     'Masculine: Jacket preferred, tie optional.', 
                     'Feminine: Dress below the knee, evening fabrics preferred.',
                     'There is no specific colour scheme. Please refrain from wearing white or very light-coloured dresses.'
-                    // add in wedding party colours
                 ]} 
             />
             <QnA 
@@ -68,5 +80,83 @@ export const FaqTab = () => {
                 answers={['Email us at brooklynaiden2026@gmail.com.']} 
             />
         </Content>
+        : <Content>
+            <h3 className={styles.contentText}>
+                {'Frequently Asked Questions'}
+            </h3>
+            <Bar/>
+            <div className={styles.contentTwoColumn}>
+                <div className={styles.contentColumn}>
+                    <QnA 
+                        question={'Where can I RSVP?'} 
+                        answers={['There will be an RSVP form available on this site in the spring once the physical invitations are mailed out.']} 
+                    />
+                    <QnA
+                        question={'When should I arrive?'}
+                        answers={[
+                            'Ceremony - 4pm start - Arrive 15 to 30 minutes prior.',
+                            'Dinner - 6pm start - Arrive 10 to 15 minutes prior.',
+                            'Reception - 8pm start'
+                        ]}
+                    />
+                    <QnA 
+                        question={'Will the wedding be indoors or outdoors?'} 
+                        answers={['The ceremony will be outdoors. Cocktail hour is split between an indoor space and an outdoor patio. Dinner and reception are indoors, with the outdoor patio remaining open. In the event of rain, the ceremony will be held indoors.']} 
+                    />
+                    <QnA 
+                        question={'What is the recommended attire?'} 
+                        answers={[
+                            'Formal or cocktail wedding attire.', 
+                            'Masculine: Jacket preferred, tie optional.', 
+                            'Feminine: Dress below the knee, evening fabrics preferred.',
+                            'There is no specific colour scheme. Please refrain from wearing white or very light-coloured dresses.'
+                        ]} 
+                    />
+                    <QnA
+                        question={'Will the wedding be child-free?'}
+                        answers={['We will be including children and babies in the celebration. All invited members of your family will be listed on your invitation.']}
+                    />
+                    <QnA 
+                        question={'Is there parking at the venue?'} 
+                        answers={['There is parking on-site. Guests can also leave vehicles parked overnight.']} 
+                    />
+                </div>
+                <div className={styles.contentColumn}>
+                     <QnA 
+                        question={'What are the wedding colours?'} 
+                        answers={[
+                            'The wedding colours are juniper and periwinkle.',
+                            'The bridesmaids will be wearing juniper (green), and the groomsmen will be wearing gray.'
+                        ]} 
+                    />
+                    <QnA 
+                        question={'Is everything at the same location?'} 
+                        answers={['Yes. The ceremony, cocktail hour, dinner, and reception are all at the Listowel Golf Club.']} 
+                    />
+                    <QnA 
+                        question={'Will the ceremony be unplugged?'} 
+                        answers={['Yes, please!']} 
+                    />
+                    <QnA 
+                        question={'Can I bring a plus one?'} 
+                        answers={['Invitations will explictly list all invited guests and plus ones.']} 
+                    />
+                    <QnA 
+                        question={'What if I have dietary restrictions?'} 
+                        answers={['Guests will be able to make meal selections when they RSVP. Vegan and vegetarian options will be available. Please contact us directly if you have severe or life-threatening food allergies. We will work with you and the venue to ensure your plated dinner is safe for consumption.']} 
+                    />
+                    <QnA 
+                        question={'Is there any form of public transit?'} 
+                        answers={['There are not reliable Ubers, taxis, or buses in Listowel. Instead, transporation is being provided. See the \'Transportation\' tab for more details.']} 
+                    />
+                    <QnA 
+                        question={'I have a question not covered here, how can I ask it?'} 
+                        answers={['Email us at brooklynaiden2026@gmail.com.']} 
+                    />
+                </div>
+            </div>
+        </Content>
+        }
+        </>
     );
 }

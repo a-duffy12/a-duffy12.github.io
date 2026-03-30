66272baab49d21f39a09c6f27abfcda05fa19294
Aiden <12aduffy@gmail.com>
2026-03-30 19:13:37 -0400

    rsvp form schema

--- src/constants.ts
+++ src/constants.ts
@@ -23,16 +23,53 @@ export const stagAndDoeDate = new Date(2026, 5, 27, 19, 0);
 export const registryLink = 'https://www.myregistry.com/wedding-registry/aiden-duffy-and-brooklyn-wright-wingham-on/5068274';
 
 export enum RsvpCode {
-    CER001, // ceremony tier individual with +1
-    CER002, // ceremony tier couple
-    CER003, // ceremony tier couple with child
-    CER005, // ceremony tier individual with +1 and two children
-    DIN001, // dinner tier individual with +1
-    DIN002, // dinner tier couple
-    DIN003, // dinner tier couple with child
-    DIN004, // dinner tier couple with two children
-    REC001, // reception tier individual with +1
-    REC002, // reception tier couple
-    REC003, // reception tier couple with child
-    REC005, // reception tier individual with +1 and two children
+    CER001 = 'CER001', // ceremony tier individual with +1
+    CER002 = 'CER002', // ceremony tier couple
+    CER003 = 'CER003', // ceremony tier couple with child
+    CER005 = 'CER005', // ceremony tier individual with +1 and two children
+    DIN001 = 'DIN001', // dinner tier individual with +1
+    DIN002 = 'DIN002', // dinner tier couple
+    DIN003 = 'DIN003', // dinner tier couple with child
+    DIN004 = 'DIN004', // dinner tier couple with two children
+    REC001 = 'REC001', // reception tier individual with +1
+    REC002 = 'REC002', // reception tier couple
+    REC003 = 'REC003', // reception tier couple with child
+    REC005 = 'REC005' // reception tier individual with +1 and two children
 };
+
+export const rsvpCountMap: Record<RsvpCode, number> = {
+    [RsvpCode.CER001]: 2,
+    [RsvpCode.CER002]: 2,
+    [RsvpCode.CER003]: 3,
+    [RsvpCode.CER005]: 4,
+    [RsvpCode.DIN001]: 2,
+    [RsvpCode.DIN002]: 2,
+    [RsvpCode.DIN003]: 3,
+    [RsvpCode.DIN004]: 4,
+    [RsvpCode.REC001]: 2,
+    [RsvpCode.REC002]: 2,
+    [RsvpCode.REC003]: 3,
+    [RsvpCode.REC005]: 4
+};
+
+export const rsvpDinnerMap: Record<RsvpCode, boolean> = {
+    [RsvpCode.CER001]: true,
+    [RsvpCode.CER002]: true,
+    [RsvpCode.CER003]: true,
+    [RsvpCode.CER005]: true,
+    [RsvpCode.DIN001]: true,
+    [RsvpCode.DIN002]: true,
+    [RsvpCode.DIN003]: true,
+    [RsvpCode.DIN004]: true,
+    [RsvpCode.REC001]: false,
+    [RsvpCode.REC002]: false,
+    [RsvpCode.REC003]: false,
+    [RsvpCode.REC005]: false
+};
+
+export enum DinnerChoice {
+    Meat1 = 'Chicken dish', // TBD
+    Meat2 = 'Fish dish', // TBD
+    Vegan = 'Vegan', // vegan TBD
+    Kids = 'Kids (chicken fingers)' // chicken fingers
+}
--- src/hooks/useRSVP.ts
+++ src/hooks/useRSVP.ts
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { z } from 'zod';
+import { DinnerChoice, RsvpCode, rsvpCountMap, rsvpDinnerMap } from '../constants';
 
 const rsvpKey = 'rsvp';
 
@@ -20,6 +22,44 @@ export const useRsvp = () => {
     };
 };
 
-const rsvpSchema = {};
+const rsvpSchema = z.object({
+    code: z.enum(RsvpCode, {
+        error: () => ({ message: 'Please enter a valid code. It can be found on your invitation.'})
+    }),
+    rsvps: z.array(
+        z.object({
+            name: z.string().min(1, 'Name is required'),
+            attending: z.boolean(),
+            meal: z.enum(DinnerChoice).optional(),
+            dietaryNeeds: z.string().optional()
+        })
+    )
+}).superRefine((data, ctx) => {
+    const expectedCount = rsvpCountMap[data.code];
+    const actualCount = data.rsvps.length;
+
+    if (expectedCount !== actualCount) {
+        ctx.addIssue({
+            code: 'custom',
+            message: `Please provide RSVP details for the ${expectedCount} invitees.`,
+            path: ['rsvps']
+        })
+    }
+
+
+    const atDinner = rsvpDinnerMap[data.code];
+
+    if (atDinner) {
+        data.rsvps.forEach((guest, index) => {
+            if (guest.attending && !guest.meal) {
+                ctx.addIssue({
+                    code: 'custom',
+                    message: 'Meal choice is required',
+                    path: ['rsvps', index, 'meal']
+                })
+            }
+        })
+    }
+});
 
 const rsvpDefaultValues = {};

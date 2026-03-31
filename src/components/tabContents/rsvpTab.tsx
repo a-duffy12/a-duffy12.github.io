52734e52b8a75a0941115ee3e59859716f508146
Aiden <12aduffy@gmail.com>
2026-03-31 19:57:44 -0400

    rsvp form, form methods, rearrange schema code locations

--- src/components/tabContents/rsvpTab.tsx
+++ src/components/tabContents/rsvpTab.tsx
@@ -1,7 +1,46 @@
-import { useRsvp } from "../../hooks/useRSVP";
+import { useRsvp } from '../../hooks/useRSVP';
+import { useForm, useFieldArray } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { RsvpData, rsvpDefaultValues, rsvpSchema, } from '../../helpers/rsvpSchema';
+import { useEffect, useMemo, useState } from 'react';
+import { rsvpCountMap, rsvpDinnerMap, rsvpPlusOneMap } from '../../constants';
 
 export const RsvpTab = () => {
-    const {rsvpSubmitted, setRsvpSubmitted, rsvpForm} = useRsvp();
+    const { rsvpSubmitted, setRsvpSubmitted } = useRsvp();
+    const { control, handleSubmit, watch, formState: { errors } } = useForm<RsvpData>({
+        resolver: zodResolver(rsvpSchema),
+        defaultValues: rsvpDefaultValues
+    });
+
+    const { fields, replace } = useFieldArray({
+        control,
+        name: 'rsvps'
+    });
+
+    const watchedCode = watch('code');
+    const watchedPlusOne = watch('bringingPlusOne');
+    const showDinnerFields = useMemo(() => rsvpDinnerMap[watchedCode], [ rsvpDinnerMap, watchedCode ]);
+    const showPlusOneForm = useMemo(() => rsvpPlusOneMap[watchedCode] && watchedPlusOne, [ rsvpPlusOneMap, watchedCode, watchedPlusOne ]);
+
+    useEffect(() => {
+        if (watchedCode && rsvpCountMap[watchedCode]) {
+            const rsvpsCount = rsvpCountMap[watchedCode] + (watchedPlusOne ? 1 : 0);
+
+            const refreshedRsvps = Array(rsvpsCount).fill(null).map(() => ({
+                name: '',
+                attending: true,
+                meal: undefined,
+                dietaryNeeds: ''
+            }));
+            replace(refreshedRsvps);
+        }
+    }, [ watchedCode, rsvpCountMap, replace ]);
+
+    const onsubmit = (data: RsvpData) => {
+        // TODO send email
+        setRsvpSubmitted(true);
+    };
+
 
     return (
         <div>RSVP</div>
--- src/hooks/useRSVP.ts
+++ src/hooks/useRSVP.ts
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react';
-import { z } from 'zod';
-import { DinnerChoice, RsvpCode, rsvpCountMap, rsvpDinnerMap, rsvpPlusOneMap } from '../constants';
 
 const rsvpKey = 'rsvp';
 
@@ -13,64 +11,8 @@ export const useRsvp = () => {
         localStorage.setItem(rsvpKey, String(rsvpSubmitted));
     }, [ rsvpSubmitted ]);
 
-    const rsvpForm = {};
-
     return {
         rsvpSubmitted,
-        setRsvpSubmitted,
-        rsvpForm
+        setRsvpSubmitted
     };
 };
-
-const rsvpSchema = z.object({
-    code: z.enum(RsvpCode, {
-        error: () => ({ message: 'Please enter a valid code. It can be found on your invitation.'})
-    }),
-    bringingPlusOne: z.boolean(),
-    rsvps: z.array(
-        z.object({
-            name: z.string().min(1, 'Name is required'),
-            attending: z.boolean(),
-            meal: z.enum(DinnerChoice).optional(),
-            dietaryNeeds: z.string().optional()
-        })
-    )
-}).superRefine((data, ctx) => {
-    const canHavePlusOne = rsvpPlusOneMap[data.code];
-    if (data.bringingPlusOne && !canHavePlusOne) {
-        ctx.addIssue({
-            code: 'custom',
-            message: 'This invitation is not eligible for a plus one.',
-            path: ['bringingPlusOne']
-        });
-    }
-    
-    const expectedCount = rsvpCountMap[data.code] + (data.bringingPlusOne ? 1 : 0);
-    const actualCount = data.rsvps.length;
-
-    if (expectedCount !== actualCount) {
-        ctx.addIssue({
-            code: 'custom',
-            message: `Please provide RSVP details for the ${expectedCount} invitees.`,
-            path: ['rsvps']
-        });
-    }
-
-    const atDinner = rsvpDinnerMap[data.code];
-
-    if (atDinner) {
-        data.rsvps.forEach((guest, index) => {
-            if (guest.attending && !guest.meal) {
-                ctx.addIssue({
-                    code: 'custom',
-                    message: 'Meal choice is required',
-                    path: ['rsvps', index, 'meal']
-                });
-            }
-        });
-    }
-});
-
-type RsvpData = z.infer<typeof rsvpSchema>;
-
-const rsvpDefaultValues = {};
